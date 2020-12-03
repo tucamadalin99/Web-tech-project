0c664836f0abb922f8fdfeffdccdf75327966181
Cezin <cezin.cupii@extendstudio.com>
2020-12-04 00:10:59 +0200

    Front end setup + Login + Register

---
+++ front-end/waste-app/src/App.js
@@ -0,0 +1,12 @@
+import './style.scss';
+import React from 'react';
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+  <Login />
+  );
+}
+
+export default App;
